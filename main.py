8030392faf0a3c2d36b4a8f35c0e2050f0994cc4
2005884 <2005884@brunel.ac.uk>
2021-10-26 14:57:13 +0100

    Commit #2

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("Yeah")
+# myList = ["blue", "yellow", "green"]
+#
+# for item in myList:
+#     if item == "yellow":
+#         print("I like that colour!")
+#         continue
+#
+#     print(item, end="\t")
+
+
+# myList = []
+#
+# for item in range(10):
+#     myList.append(item)
+#
+# print(myList)
+
+
+# for item in "Python":
+#     if item == "t":
+#         continue
+#     print(item, end="")
+
+
+counter = 5
+
+while counter > 0:
+    print(counter)
+    counter -= 1
